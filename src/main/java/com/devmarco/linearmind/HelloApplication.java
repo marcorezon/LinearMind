0f9eb420d2302f509f38bd2333e9612b4c37ce9c
Marco Rêzon <marcorezonmoura1@gmail.com>
2025-04-21 06:43:40 -0300

    feat: added modal to set clock parameters

--- src/main/java/com/devmarco/linearmind/HelloApplication.java
+++ src/main/java/com/devmarco/linearmind/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/devmarco/linearmind/fxml/core.fxml"));
-         Scene scene = new Scene(fxmlLoader.load(), 350, 350);
+        Scene scene = new Scene(fxmlLoader.load(), 350, 350);
         stage.setTitle("LinearMind");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/devmarco/linearmind/controllers/SettingsController.java
+++ src/main/java/com/devmarco/linearmind/controllers/SettingsController.java
@@ -1,4 +1,44 @@
 package com.devmarco.linearmind.controllers;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.MenuItem;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
 public class SettingsController {
+    @FXML
+    private MenuItem clockSettings;
+
+    @FXML
+    private MenuItem blockWebsite;
+
+    @FXML
+    private MenuItem blockApplication;
+
+    public void displayClockSettingsModal() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/devmarco/linearmind/fxml/modal/clockSettingsModal.fxml"));
+            Parent root = fxmlLoader.load();
+
+            Stage modalStage = new Stage();
+            modalStage.initStyle(StageStyle.UNDECORATED);
+            modalStage.initModality(Modality.APPLICATION_MODAL);
+            modalStage.setTitle("Clock Settings");
+
+            Scene scene = new Scene(root);
+            modalStage.setScene(scene);
+
+            modalStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.err.println("Erro ao carregar o modal: " + e.getMessage());
+        }
+    }
 }
+
